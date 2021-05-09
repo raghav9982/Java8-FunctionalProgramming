65b9f337c6212a26885f5d0bcddfe859562e58de
Venkat Purimetla <raghav9982@gmail.com>
2021-05-08 21:57:35 -0500

    formatted the file

--- src/practice/interview/BuildMapAndSortByValues.java
+++ src/practice/interview/BuildMapAndSortByValues.java
@@ -9,13 +9,13 @@ import java.util.stream.Collectors;
 
 public class BuildMapAndSortByValues {
     public static void main(String[] args) {
-        Character [] arr = {'a','d','a','k','b','h','b','a', 'b', 'a', 'k'};
-        Map<Character,Integer>  characterIntegerMap = new HashMap<>();
-        Arrays.asList(arr).forEach(x-> characterIntegerMap.put(x,characterIntegerMap.getOrDefault(x,0)+1));
+        Character[] arr = {'a', 'd', 'a', 'k', 'b', 'h', 'b', 'a', 'b', 'a', 'k'};
+        Map<Character, Integer> characterIntegerMap = new HashMap<>();
+        Arrays.asList(arr).forEach(x -> characterIntegerMap.put(x, characterIntegerMap.getOrDefault(x, 0) + 1));
         Map<Character, Integer> sortedHashMap = characterIntegerMap.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,(e1,e2)-> e1, LinkedHashMap::new));
-        sortedHashMap.forEach((k,v)-> System.out.println("key: "+ k + " value: "+ v));
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        sortedHashMap.forEach((k, v) -> System.out.println("key: " + k + " value: " + v));
 
     }
 }
