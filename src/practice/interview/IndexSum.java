aeba099260e2a0bbab3578b22627f9a5c924a723
Venkat Purimetla <raghav9982@gmail.com>
2021-05-12 21:32:01 -0500

    refactored the code

---
+++ src/practice/interview/IndexSum.java
@@ -0,0 +1,25 @@
+package practice.interview;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class IndexSum {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 2, 5, 7};
+        int target = 6;
+        int[] outputArr = new int[2];
+
+        Map<Integer, Integer> complimentMap = new LinkedHashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (complimentMap.containsKey(target - arr[i])) {
+                outputArr[0] = complimentMap.get(target - arr[i]);
+                outputArr[1] = i;
+            }
+            complimentMap.putIfAbsent(arr[i], i);
+        }
+
+
+        System.out.println("first val" + outputArr[0] + " second Val : " + outputArr[1]);
+    }
+}
